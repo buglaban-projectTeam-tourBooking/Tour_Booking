fc3edbb75f4ab38e766440d06a5eae377215ee61
duong <leethuanduong@gmail.com>
2026-03-20 00:22:14 +0700

    feat: danh sach tour

--- Front_end/src/App.jsx
+++ Front_end/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TourList from "./pages/TourList";
 import TourDetail from "./pages/TourDetail";
 
 function App() {
   return (
-    <div className="app">
-      <TourDetail />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<TourList />} />
+          <Route path="/tour/:id" element={<TourDetail />} />
+          <Route path="/tour/domestic" element={<TourList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
